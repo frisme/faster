5a39840906dc79e8c897cdc1319a5888e12502cf
Caio <galaocomtenis@gmail.com>
2024-11-06 09:52:45 -0300

    arrumando errors de animacao

--- faster/MainPage.xaml.cs
+++ faster/MainPage.xaml.cs
@@ -10,6 +10,12 @@ public partial class MainPage : ContentPage
 	int velocidade2 = 0;
 	int velocidade3 = 0;
 	int velocidade4 = 0;
+	int velocidade5 = 0;
+	int velocidade6 = 0;
+	int velocidade7 = 0;
+	int velocidade8 = 0;
+	int velocidade9 = 0;
+	int velocidade10 = 0;
 	int larguraJanela = 0;
 	int alturaJanela = 0;
 
@@ -17,6 +23,21 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenha();
+	}
+
+	async Task Desenha()
+	{
+		while(!EstaMorto)
+		{
+			GerenciarCenarios();
+			await Task.Delay(TempoEntreFrames);
+		}
 	}
 
 	
@@ -33,16 +54,156 @@ public partial class MainPage : ContentPage
 		velocidade2 = (int)(w * 0.004);
 		velocidade3 = (int)(w * 0.008);
 		velocidade4 = (int)(w * 0.01);
+		velocidade5 = (int)(w * 0.01);
+		velocidade6 = (int)(w * 0.01);
+		velocidade7 = (int)(w * 0.01);
+		velocidade8 = (int)(w * 0.01);
+		velocidade9 = (int)(w * 0.01);
+		velocidade10 = (int)(w * 0.01);
 	}
 
-	void CorrigeTamanhoCenario(double w, double h)
+	void CorrigeTamanhoCenario(double w, double height)
 	{
 		foreach (var a in primeiro.Children)
 			(a as Image).WidthRequest = w;
-		foreach (var b in terceiro.Children)
+		foreach (var b in segundo.Children)
 			(b as Image).WidthRequest = w;
+		foreach (var c in terceiro.Children)
+		    (c as Image).WidthRequest = w;
+		foreach (var d in quarto.Children)
+		    (d as Image).WidthRequest = w;
+		foreach (var e in quinto.Children)
+		    (e as Image).WidthRequest = w;
+		foreach (var f in sexto.Children)
+		    (f as Image).WidthRequest = w;
+		foreach (var g in setimo.Children)
+		    (g as Image).WidthRequest = w;
+		foreach (var h in oitavo.Children)
+		    (h as Image).WidthRequest = w;
+		foreach (var i in nono.Children)
+		    (i as Image).WidthRequest = w;
+		foreach (var j in decimo.Children)
+		    (j as Image).WidthRequest = w;
 
 		primeiro.WidthRequest = w * 1.5;
+		segundo.WidthRequest = w * 1.5;
 		terceiro.WidthRequest = w * 1.5;
+		quarto.WidthRequest = w * 1.5;
+		quinto.WidthRequest = w * 1.5;
+		sexto.WidthRequest = w * 1.5;
+		setimo.WidthRequest = w * 1.5;
+		oitavo.WidthRequest = w * 1.5;
+		nono.WidthRequest = w * 1.5;
+		decimo.WidthRequest = w * 1.5;
+	}
+
+	void GerenciarCenarios()
+	{
+		MoveCenario();
+		GerenciaCenario(primeiro);
+		GerenciaCenario(segundo);
+		GerenciaCenario(terceiro);
+		GerenciaCenario(quarto);
+		GerenciaCenario(quinto);
+		GerenciaCenario(sexto);
+		GerenciaCenario(setimo);
+		GerenciaCenario(oitavo);
+		GerenciaCenario(nono);
+		GerenciaCenario(decimo);		
+	}
+
+	void MoveCenario()
+	{
+		primeiro.TranslationX -= velocidade;
+		segundo.TranslationX -= velocidade2;
+		terceiro.TranslationX -= velocidade3;
+		quarto.TranslationX -= velocidade4;
+		quinto.TranslationX -= velocidade5;
+		sexto.TranslationX -= velocidade6;
+		setimo.TranslationX -= velocidade7;
+		oitavo.TranslationX -= velocidade8;
+		nono.TranslationX -= velocidade9;
+		decimo.TranslationX -= velocidade10;
+	}
+
+	void GerenciaCenario(HorizontalStackLayout hsl)
+	{
+		var view = (hsl.Children.First() as Image);
+
+		if(view.WidthRequest + hsl.TranslationX < 0)
+		{
+			hsl.Children.Remove(view);
+			hsl.Children.Add(view);
+			hsl.TranslationX = view.TranslationX;
+		}
+	}
+
+	public class Animacao
+	{
+		protected List<String> Animacao1 = new List<String>();
+		protected List<String> Animacao2 = new List<String>();
+		protected List<String> Animacao3 = new List<String>();
+		protected bool Loop = true;
+		protected int AnimacaoAtiva = 1;
+		bool parado = true;
+		int frameAtual = 1;
+		protected Image compImage;
+		public Animacao(Image a)
+		{
+			compImage = a;
+		}
+
+	public void Stop()
+	{
+		parado = true;
+	}
+
+	public void Play()
+	{
+		parado = false;
+	}
+
+	public void SetAnimacaoAtiva(int a)
+	{
+		AnimacaoAtiva = a;
+	}
+
+	public void Desenha()
+	{
+		if (parado)
+		   return;
+		string nomeArquivo;
+		int tamanhoAnimacao;
+		if (AnimacaoAtiva == 1)
+		{
+			nomeArquivo = Animacao1 [frameAtual];
+			tamanhoAnimacao = Animacao1.Count;
+		}
+		else if (AnimacaoAtiva == 2)
+		{
+			nomeArquivo = Animacao2[frameAtual];
+			tamanhoAnimacao = Animacao2.Count;
+		}
+		else if (AnimacaoAtiva == 3)
+		{
+			nomeArquivo = Animacao3[frameAtual];
+			tamanhoAnimacao = Animacao3.Count;
+		}
+		compImagem.Source = ImageSource.FromFile(nomeArquivo);
+		frameAtual++;
+		if (frameAtual >= tamanhoAnimacao)
+		{
+			if(Loop)
+			   frameAtual = 0;
+			else
+			{
+				parado = true;
+				QuandoParar();
+			}
+		}
+	}	
+}
+	public virtual void QuandoParar()
+	{
 	}
 }
